28f38a4fc3b2631a70ec644ed245c255ca6bc504
Keith Fuller <keith66fuller@gmail.com>
2018-07-20 18:08:03 -0500

    dbadmin table is working, integrating MUI Grid now

--- client/src/pages/DbAdmin/DbAdmin.js
+++ client/src/pages/DbAdmin/DbAdmin.js
@@ -1,27 +1,45 @@
 import React, { Component } from "react";
-import { List, ListItem } from "../../components/List";
 import ReactTable from "react-table";
 import 'react-table/react-table.css';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import API from "../../utils/API";
 
 class DbAdmin extends Component {
-  state = {
-    pubs: [],
-    hasError: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      pubs: [],
+      selected: {},
+      selectAll: 0
+    }
+    this.toggleRow = this.toggleRow.bind(this);
   }
-
-  componentDidCatch(error, info) {
-    // Display fallback UI
-    this.setState({ hasError: true });
-    // You can also log the error to an error reporting service
-    console.log(error, info);
+  toggleRow(_id) {
+    const newSelected = Object.assign({}, this.state.selected);
+    newSelected[_id] = !this.state.selected[_id];
+    this.setState({
+      selected: newSelected,
+      selectAll: 2
+    });
   }
-
+  toggleSelectAll() {
+    let newSelected = {};
+    if (this.state.selectAll === 0) {
+      this.state.pubs.forEach(x => {
+        newSelected[x._id] = true;
+      });
+    }
+    this.setState({
+      selected: newSelected,
+      selectAll: this.state.selectAll === 0 ? 1 : 0
+    });
+  }
   componentDidMount() {
     this.loadPubs();
   }
 
-  // Loads all Pubs  and sets them to this.state.Pubs
   loadPubs = () => {
     API.getpubs()
       .then(res => {
@@ -33,27 +51,83 @@ class DbAdmin extends Component {
   };
 
 
+  
   render() {
+
+    const styles = theme => ({
+      root: {
+        flexGrow: 1,
+      },
+      paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'left',
+        color: theme.palette.text.secondary,
+      },
+    });
 
     const columns = [
       {
         Header: 'Pub',
-        accessor: 'name'
+        accessor: 'name',
+        Cell: row => (
+          <div style={{ backgroundColor: this.state.selected[row.original._id] ? "#dadada" : "" }}>
+            {row.value}
+          </div>
+        )
       }, {
         Header: 'Categories',
         id: 'categories',
         accessor: d => d.categories.map(e => e.alias).sort().join(" "),
-        filterable: true
+        filterable: true,
+        Cell: row => (
+          <div style={{ backgroundColor: this.state.selected[row.original._id] ? "#dadada" : "" }}>
+            {row.value}
+          </div>
+        )
       }
     ]
 
     return (
-      <ReactTable
-        data={this.state.pubs}
-        columns={columns}
-      />
+        <Grid container spacing={24}>
+          <Grid item xs>
+            <Paper>
+              <ReactTable
+                data={this.state.pubs}
+                columns={columns}
+                // When any part of a row is clicked.
+                getTdProps={(state, rowInfo, column, instance) => {
+                  return {
+                    onClick: (e, handleOriginal) => {
+                      console.log("A Td Element was clicked!");
+                      // console.log("it produced this event:", e);
+                      // console.log("It was in this column:", column);
+                      // console.log("It was in this row:", rowInfo);
+                      // console.log("It was in this table instance:", instance);
+                      console.log(`DB instance _id: ${rowInfo.original._id}`)
+                      this.toggleRow(rowInfo.original._id)
+                      // IMPORTANT! React-Table uses onClick internally to trigger
+                      // events like expanding SubComponents and pivots.
+                      // By default a custom 'onClick' handler will override this functionality.
+                      // If you want to fire the original onClick handler, call the
+                      // 'handleOriginal' function.
+                      if (handleOriginal) {
+                        handleOriginal();
+                      }
+                    }
+                  };
+                }}
+              />
+            </Paper>
+          </Grid>
+          {this.state.selected.length ? (
+            <Grid item xs>
+            <Paper>xs</Paper>
+          </Grid>
+          ) : null}
+        </Grid>
     );
   }
 }
 
 export default DbAdmin;
+
