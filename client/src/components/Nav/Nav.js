9a3a48d073c6d8ae892590d06197256fd76ebe1b
Jason O'Higgins <jason.ohiggins@gmail.com>
2018-07-21 12:13:14 -0500

    Navbar and Home page link updates
    Navbar links now responsive and in dropdown, also icons in dashboard on home page are now activce links.

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,7 +3,10 @@ import React from "react";
 const Nav = () => (
 <nav className="white" role="navigation">
     <div className="nav-wrapper container">
-      <a id="logo-container" href="/Home" className="brand-logo gold-text">BUD'S N' SUDS</a>
+      <a id="logo-container" href="/Home" className="brand-logo gold-text">BUD'S N' SUDS</a>      <ul className="right hide-on-med-and-down">
+        <li><a href="/Profile">Profile</a></li>
+      </ul>
+
       <ul className="right hide-on-med-and-down">
         <li><a href="/Crawls">Crawls</a></li>
       </ul>
@@ -16,19 +19,12 @@ const Nav = () => (
         <li><a href="/Buds">Buds</a></li>
       </ul>
 
-
       <ul id="nav-mobile" className="sidenav">
         <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-
-      <ul id="nav-mobile" className="sidenav">
-        <li><a href="#">Navbar Link</a></li>
-      </ul>
-      <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-
-      <ul id="nav-mobile" className="sidenav">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="/Buds">Buds</a></li>
+        <li><a href="/Pubs">Pubs</a></li>
+        <li><a href="/Crawls">Crawls</a></li>
+        <li><a href="/Profile">Profile</a></li>
       </ul>
       <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
     </div>
