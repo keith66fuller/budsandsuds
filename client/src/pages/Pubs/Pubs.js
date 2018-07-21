8ff4c361bab14b2744c55edae12d970e1d4cad09
Keith Fuller <36208707+keith66fuller@users.noreply.github.com>
2018-07-21 10:11:46 -0500

    Copied DBAdmin into Pubs

--- client/src/pages/Pubs/Pubs.js
+++ client/src/pages/Pubs/Pubs.js
@@ -1,21 +1,169 @@
 import React, { Component } from "react";
-import Banner from "../../components/Banner";
-import PubsComp from "../../components/PubsComp";
-import Maps from "../../components/Maps";
-import { Link } from 'react-router';
+import ReactTable from "react-table";
+import 'react-table/react-table.css';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import API from "../../utils/API";
+import { List, ListItem } from "../../components/List";
 
 class Pubs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pubs: [],
+      selected: {},
+      selectAll: 0
+    }
+    this.toggleRow = this.toggleRow.bind(this);
+  }
+  toggleRow(_id) {
+    const newSelected = Object.assign({}, this.state.selected);
+    newSelected[_id] = !this.state.selected[_id];
+    this.setState({
+      selectAll: 2,
+      selected: newSelected
+    });
 
-  render() {
-    return (
-      <React.Fragment>
-        <Banner />
-        <PubsComp />
-        {/* <Maps /> */}
-      </React.Fragment>
-    )
+    console.log(Object.keys(this.state.selected))
+
+  }
+  toggleSelectAll() {
+    let newSelected = {};
+    if (this.state.selectAll === 0) {
+      this.state.pubs.forEach(x => {
+        newSelected[x._id] = true;
+      });
+    }
+    this.setState({
+      selected: newSelected,
+      selectAll: this.state.selectAll === 0 ? 1 : 0
+    });
+  }
+  componentDidMount() {
+    this.loadPubs();
   }
 
-};
+  loadPubs = () => {
+    API.getpubs()
+      .then(res => {
+        this.setState({
+          pubs: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+
+
+  render() {
+
+    const styles = theme => ({
+      root: {
+        flexGrow: 1,
+      },
+      paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'left',
+        color: theme.palette.text.secondary,
+      },
+    });
+
+    const columns = [
+      {
+        Header: 'Pub',
+        accessor: 'name',
+        Cell: row => (
+          <div style={{ backgroundColor: this.state.selected[row.original._id] ? "#dadada" : "" }}>
+            {row.value}
+          </div>
+        )
+      }, {
+        Header: 'Categories',
+        id: 'categories',
+        accessor: d => d.categories.map(e => e.alias).sort().join(" "),
+        filterable: true,
+        Cell: row => (
+          <div style={{ backgroundColor: this.state.selected[row.original._id] ? "#dadada" : "" }}>
+            {row.value}
+          </div>
+        )
+      }
+    ]
+
+    const columnsSel = [
+      {
+        Header: 'Pub',
+        accessor: 'name',
+      }, {
+        Header: 'Categories',
+        accessor: 'categories',
+      }
+    ]
+
+    return (
+        <Grid container spacing={24}>
+          <Grid item xs>
+            <Paper>
+              <ReactTable
+                data={this.state.pubs}
+                columns={columns}
+                // When any part of a row is clicked.
+                getTdProps={(state, rowInfo, column, instance) => {
+                  return {
+                    onClick: (e, handleOriginal) => {
+                      console.log("A Td Element was clicked!");
+                      // console.log("it produced this event:", e);
+                      // console.log("It was in this column:", column);
+                      // console.log("It was in this row:", rowInfo);
+                      // console.log("It was in this table instance:", instance);
+                      console.log(`DB instance _id: ${rowInfo.original._id}`)
+                      this.toggleRow(rowInfo.original._id)
+                      // IMPORTANT! React-Table uses onClick internally to trigger
+                      // events like expanding SubComponents and pivots.
+                      // By default a custom 'onClick' handler will override this functionality.
+                      // If you want to fire the original onClick handler, call the
+                      // 'handleOriginal' function.
+                      if (handleOriginal) {
+                        handleOriginal();
+                      }
+                    }
+                  };
+                }}
+              />
+            </Paper>
+          </Grid>
+          {Object.keys(this.state.selected).find(e => this.state.selected[e] === true) ? (
+            <Grid item xs>
+            <Paper>
+            <List>
+              {Object.keys(this.state.selected).map(
+                k => {
+                  if (this.state.selected[k] === true) {
+                    const pub = this.state.pubs.find(
+                      e => {
+                        return e._id === k
+                      }
+                    )
+                    return (
+                      <ListItem key={pub._id}>
+                        <strong>
+                          {pub.name}
+                        </strong>
+                    </ListItem>
+                    )
+                  }
+
+                }
+              )}
+              </List>
+            </Paper>
+          </Grid>
+          ) : null}
+        </Grid>
+    );
+  }
+}
 
 export default Pubs;
+
