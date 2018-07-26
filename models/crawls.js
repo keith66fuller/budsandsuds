73e7e5cea614d21d1d17d44c8b6389907ef80642
Keith Fuller <keith66fuller@gmail.com>
2018-07-26 10:36:03 -0500

    can create crawl and put into DB.

--- models/crawls.js
+++ models/crawls.js
@@ -6,12 +6,15 @@ mongoose.set('debug', true)
 
 
 const crawlsSchema = new Schema({
-  name: { type: String},
-  createdBy: { type: Object},
-  pubs: {
+  name: { type: Schema.Types.String},
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: "Buds"
+    },
+  pubs: [{
     type: Schema.Types.ObjectId,
     ref: "pubs"
-    },
+    }],
   buds: {
     type: Schema.Types.ObjectId,
     ref: "Buds"
@@ -21,11 +24,11 @@ const crawlsSchema = new Schema({
                   type: Schema.Types.ObjectId,
                   ref: "Buds"
                   },
-                comment: String,
-                rating: Number,
-                dateCreated:  { type: Date, default: Date.now }
+                comment: Schema.Types.String,
+                rating: Schema.Types.Number,
+                dateCreated:  { type: Schema.Types.Date, default: Date.now }
           }],
-  dateCreated: { type: Date, default: Date.now }
+  dateCreated: { type: Schema.Types.Date, default: Date.now }
 });
 
 const Crawl = mongoose.model("Crawl", crawlsSchema);
