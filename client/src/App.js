29257d3f61f1369b829e6560e65e6d14a2f83201
Keith Fuller <keith66fuller@gmail.com>
2018-07-27 21:14:13 -0500

    converting

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,6 @@ import Crawls from "./pages/Crawls";
 import Pubs from "./pages/Pubs";
 
 
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import PasswordForget from "./pages/PasswordForget";
-import Account from "./pages/Account";
-import withAuthentication from "./components/firebaseComp/withAuthentication"
 import * as routes from "./constants/routes";
 
 
@@ -23,12 +18,6 @@ const App = () => (
       <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
-
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/pw-forget" component={PasswordForget} />
-        <Route exact path="/account" component={Account} />
-
         <Route exact path="/home" component={Home} />
         <Route exact path="/buds" component={Buds} />
         <Route exact path="/crawls" component={Crawls} />
@@ -41,5 +30,4 @@ const App = () => (
   </Router>
 );
 
-export default withAuthentication(App);
-// export default App;
+export default App;
--- client/src/firebase/firebasecred.js
+++
@@ -1,6 +0,0 @@
-export const Fire_API_KEY='AIzaSyBa5L5Ur30swsgK2FdSgkU1cKa7SCjCsJc';
-export const Fire_AUTH_DOMAIN='budsnsuds-78797.firebaseapp.com';
-export const Fire_DATABASE_URL='https://budsnsuds-78797.firebaseio.com';
-export const Fire_PROJECT_ID='budsnsuds-78797';
-export const Fire_STORAGE_BUCKET='budsnsuds-78797.appspot.com';
-export const Fire_MESSAGE_SENDER_ID='571872931370';
